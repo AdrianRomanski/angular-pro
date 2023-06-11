d32f45b5af27cc24efeeb15d3b2dc9573346266c
Adrian Romański <muszynianka1337@gmail.com>
2023-06-11 09:35:35 +0200

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,27 @@ export class AppComponent implements AfterContentInit{
   @ViewChild('entry', {read: ViewContainerRef}) entry!: ViewContainerRef;
   @ViewChild('tmpl') tmpl!: TemplateRef<any>;
 
+  items = [{
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  },{
+    name: 'Tom Delonge',
+    age: 41,
+    location: 'California'
+  },{
+    name: 'Travis Barker',
+    age: 41,
+    location: 'California'
+  }];
+
   constructor(
     private resolver: ComponentFactoryResolver,
     private dr: ChangeDetectorRef,
   ) {
+    setTimeout(() => {
+      this.items = [...this.items, { name: 'Matt Skiba', age: 40, location: 'California' }];
+    }, 2000);
   }
 
   rememberUser(remember: boolean) {
@@ -47,22 +64,22 @@ export class AppComponent implements AfterContentInit{
     console.log('Login', user, this.rememberMe);
   }
   ngAfterContentInit(): void {
-      setTimeout(() => {
-        const factory = this.resolver.resolveComponentFactory(AuthFormComponent)
-
-        this.component  = this.entry.createComponent(factory);
-        // @ts-ignore
-        this.component.instance['title'] = 'Dynamic title';
-
-        this.component.instance.submitted.subscribe(this.loginUser);
-
-        this.dr.detectChanges();
-        this.entry.createEmbeddedView(this.tmpl, {
-          location: 'England, UK',
-          $implicit: 'Implicit Name'
-        });
-        this.entry.createEmbeddedView(this.tmpl);
-      })
+      // setTimeout(() => {
+      //   const factory = this.resolver.resolveComponentFactory(AuthFormComponent)
+      //
+      //   this.component  = this.entry.createComponent(factory);
+      //   // @ts-ignore
+      //   this.component.instance['title'] = 'Dynamic title';
+      //
+      //   this.component.instance.submitted.subscribe(this.loginUser);
+      //
+      //   this.dr.detectChanges();
+      //   this.entry.createEmbeddedView(this.tmpl, {
+      //     location: 'England, UK',
+      //     $implicit: 'Implicit Name'
+      //   });
+      //   this.entry.createEmbeddedView(this.tmpl);
+      // })
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AuthFormModule} from "./auth-form/auth-form.module";
+import {CreditCardDirective} from "./credit-card/credit-card.directive";
+import {TooltipDirective} from "./tooltip/tooltop.directive";
+import {MyForDirective} from "./my-for/my-for.directive";
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreditCardDirective,
+    TooltipDirective,
+    MyForDirective
   ],
   imports: [
     BrowserModule,
